8d07b8a95496941fc3bc11ad68864d4eebd874c3
Mishr1233 <shivshankarmishra84351935@gmail.com>
2025-02-22 20:25:59 +0530

    [Shiv shankar] Add. Extend user controller service

--- src/main/java/com/example/GreetingApp/service/GreetingService.java
+++ src/main/java/com/example/GreetingApp/service/GreetingService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 
-    //This method returns the simple greeting message
+    //This method returns greeting message
     public String getSimpleGreeting() {
         return "Hello World";
     }
